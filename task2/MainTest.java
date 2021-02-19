6db322030e69bafc1d2d74c58ea7de07f0ebc73f
Marloto <Marloto@users.noreply.github.com>
2021-02-19 19:33:47 +0100

    Create MainTest.java

---
+++ task2/MainTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Before;
+
+public class MainTest {
+    @Test
+    public void testStringLength() {
+        assertEquals("Test", 1, 1);
+    }
+}
