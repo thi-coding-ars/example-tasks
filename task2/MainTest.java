9c82f8e51a530738dd6ad95f261ec8f72a3ed287
Marloto <Marloto@users.noreply.github.com>
2025-03-10 10:32:53 +0100

    Update MainTest.java

--- task2/MainTest.java
+++ task2/MainTest.java
@@ -1,10 +1,26 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.After;
 
 public class MainTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
     @Test
-    public void testStringLength() {
-        assertEquals("Test", 1, 1);
+    public void testHelloWorldOutput() {
+        Main.main(new String[]{});
+        assertEquals("Hello, World!", outContent.toString().trim());
     }
 }
