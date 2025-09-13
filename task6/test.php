273eb735fd5e4822261240660f37a3cba57404dc
Marloto <marloto@codestack.de>
2025-09-13 14:35:34 +0200

    added php example task

---
+++ task6/test.php
@@ -0,0 +1,7 @@
+test('profile page contains student name and basic info', function () {
+    ob_start();
+    include 'index.php';
+    $html = ob_get_clean();
+    
+    expect($html)->toContain('Hello, World!');
+});
